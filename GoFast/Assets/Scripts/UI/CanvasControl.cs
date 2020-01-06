af4d54e91caf1e7b67f1c7692204c355e36b3942
VFelix <voelk_felix@web.de>
2020-01-06 18:27:27 +0100

    added simple main menu

--- GoFast/Assets/Scripts/UI/CanvasControl.cs
+++ GoFast/Assets/Scripts/UI/CanvasControl.cs
@@ -5,7 +5,6 @@ public class CanvasControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.enabled = false;
     }
 
     // Update is called once per frame
---
+++ GoFast/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void LoadLevelByIndex(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevelByName(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
