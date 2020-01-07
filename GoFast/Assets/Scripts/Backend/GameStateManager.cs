b0512534995919d41fb9b432fc34e766e84b0dac
VFelix <voelk_felix@web.de>
2020-01-07 13:55:59 +0100

    Added Saved Highscore

---
+++ GoFast/Assets/Scripts/Backend/GameStateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int highscore;
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("Score", 0);
+    }
+
+    void updateHighscore(int score)
+    {
+        PlayerPrefs.SetInt("Score", score);
+    }
+}
--- GoFast/Assets/Scripts/UI/MainMenuManager.cs
+++ GoFast/Assets/Scripts/UI/MainMenuManager.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public UnityEngine.UI.Button button1, button2, button3;
 
+    private void Start()
+    {
+        if(PlayerPrefs.GetInt("Score", 0) == 0)
+        {
+            button2.interactable = false;
+            button3.interactable = false;
+        }
+    }
     public void LoadLevelByIndex(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex);
